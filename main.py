892f3854da4a95f88a1ed301747f84125069ad5f
Vladimir Levin <96210239+Vavasiq@users.noreply.github.com>
2022-05-04 18:49:45 +0000

    Igor Joined

--- main.py
+++ main.py
@@ -4,22 +4,24 @@ from replit import db
 from keep_alive import keep_alive
 import time
 
-link='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
+link = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'
 db["key"] = "value"
-def getCrypto(name_crypto):
-  r = requests.get(link)
-  response=r.json()
-  for i in range (len(response)):
-      if response[i].get("id") == name_crypto:
-          print(response[i].get("name"), "-",response[i].get("current_price"))
-          return  response[i].get("current_price")
-      # elif response[i].get("id") == "solana":
-      #     return d, e = response[i].get("name"), response[i].get("current_price")  
-      #     print(response[i].get("name"), "-", response[i].get("current_price"))
-      # elif response[i].get("id") == "ethereum":
-      #     print(response[i].get("name"), "-",response[i].get("current_price"))
-      #     return f, g = response[i].get("name"), response[i].get("current_price")
 
+
+def getCrypto(name_crypto):
+    r = requests.get(link)
+    response = r.json()
+    for i in range(len(response)):
+        if response[i].get("id") == name_crypto:
+            print(response[i].get("name"), "-",
+                  response[i].get("current_price"))
+            return response[i].get("current_price")
+        # elif response[i].get("id") == "solana":
+        #     return d, e = response[i].get("name"), response[i].get("current_price")
+        #     print(response[i].get("name"), "-", response[i].get("current_price"))
+        # elif response[i].get("id") == "ethereum":
+        #     print(response[i].get("name"), "-",response[i].get("current_price"))
+        #     return f, g = response[i].get("name"), response[i].get("current_price")
 
 
 # instantiate a discord client
@@ -30,6 +32,7 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -38,13 +41,14 @@ async def on_message(message):
     if message.content.startswith('$bitcoin'):
         crypto = getCrypto("bitcoin")
         await message.channel.send(crypto)
+
+
 # @client.event
 # async def on_ready():
 #     print(f'You have logged in as {client}')
 #     channel = discord.utils.get(client.get_all_channels(), name='crypto')
 
 #     await client.get_channel(channel.id).send('bot is now online!')
-
 
 # # called whether there is a message in the chat
 # @client.event
@@ -71,12 +75,11 @@ async def on_message(message):
 #         cryptoToBeChecked = message.content.split('$support ', 1)[1].lower()
 #         await message.channel.send(isCryptoSupported(cryptoToBeChecked))
 
-    # timing = time.time()
-    # while (1):
-    #     if time.time() - timing > 10.0:
-    #         timing = time.time()
-    #         print()
-
+# timing = time.time()
+# while (1):
+#     if time.time() - timing > 10.0:
+#         timing = time.time()
+#         print()
 
 keep_alive()
 BOT_TOKEN = 'OTY5MTg2ODI4NzIyNzk0NTc2.Ympvkg.zh2R5E_yJEJA4n7MUpZudW52feE'
